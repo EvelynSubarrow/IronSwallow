9549b7306e8cb356853f98a1700de7eebe684797
Evelyn <evelyn@e-s.me.uk>
2019-12-25 22:08:42 +0000

    Automatically generate most location parts of query

--- util/query.py
+++ util/query.py
@@ -15,14 +15,17 @@ def _location_outline(target_list):
     pass
 
 def station_board(cursor, tiplocs, intermediate_tiploc=None, passenger_only=True):
+    stat_select = ""
+    for location_name, stat_name in [("base", "b_stat"), ("orig", "o_stat"), ("inter", "i_stat"), ("dest", "d_stat")]:
+        stat_select += "{ln}.type, {ln}.tiploc, {ln}.activity, {ln}.wta, {ln}.wtp, {ln}.wtd, {ln}.pta, {ln}.ptd, {ln}.cancelled,\n".format(ln=location_name)
+        for time_name in ("ta", "tp", "td"):
+            stat_select += "{sn}.{tn}, {sn}.{tn}_source, {sn}.{tn}_type, {sn}.{tn}_delayed{comma}\n".format(
+                sn=stat_name, tn=time_name, comma=","*(not(stat_name=="d_stat" and time_name=="td")))
     cursor.execute("""SELECT
         sch.uid,sch.rid,sch.rsid,sch.ssd,sch.signalling_id,sch.status,sch.category,sch.operator,
         sch.is_active,sch.is_charter,sch.is_passenger,
 
-        base.type,base.tiploc,base.activity,base.wta,base.wtp,base.wtd,base.pta,base.ptd,base.cancelled,
-        orig.type,orig.tiploc,orig.activity,orig.wta,orig.wtp,orig.wtd,orig.pta,orig.ptd,orig.cancelled,
-        inter.type,inter.tiploc,inter.activity,inter.wta,inter.wtp,inter.wtd,inter.pta,inter.ptd,inter.cancelled,
-        dest.type,dest.tiploc,dest.activity,dest.wta,dest.wtp,dest.wtd,dest.pta,dest.ptd,dest.cancelled
+        {}
 
         FROM darwin_schedule_locations AS base
         LEFT JOIN darwin_schedules AS sch ON base.rid=sch.rid
@@ -39,7 +42,7 @@ def station_board(cursor, tiplocs, intermediate_tiploc=None, passenger_only=True
         WHERE base.wtd IS NOT NULL
         AND base.tiploc in %s
         AND base.type in ('IP', 'DT', 'OR')
-        AND NOT sch.is_deleted ORDER BY base.wtd;""", (
+        AND NOT sch.is_deleted ORDER BY base.wtd;""".format(stat_select), (
         tiplocs,))
 
     services = []
