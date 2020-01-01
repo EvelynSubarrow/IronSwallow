43bffc6cd07b5c114fed3dec3fbe10321a45e28b
Evelyn <evelyn@e-s.me.uk>
2020-01-01 18:42:35 +0000

    add whitespace stripping option to xml parser

--- util/pushport.py
+++ util/pushport.py
@@ -2,11 +2,12 @@ import io, xml.sax
 from collections import OrderedDict
 
 class PushPortParser(xml.sax.ContentHandler):
-    def __init__(self, list_paths=["Pport.uR", "Pport.uR.schedule", "Pport.uR.TS"]):
+    def __init__(self, list_paths=["Pport.uR", "Pport.uR.schedule", "Pport.uR.TS"], strip_whitespace=True):
         self._path = []
         self._root = OrderedDict()
         self._dicts = [self._root]
         self._list_paths = [a.split(".") for a in list_paths]
+        self._strip_whitespace = strip_whitespace
 
     def startElement(self, name, attrs):
         name = name.split(":")[-1]
@@ -34,7 +35,8 @@ class PushPortParser(xml.sax.ContentHandler):
     def characters(self, data):
         if "$" not in self._dicts[-1]:
             self._dicts[-1]["$"] = ""
-        self._dicts[-1]["$"] += data
+        if (not data.isspace() and not self._dicts[-1]["$"].isspace()) or not self._strip_whitespace:
+            self._dicts[-1]["$"] += data
 
     def parse(self, f):
         xml.sax.parse(f, self)
