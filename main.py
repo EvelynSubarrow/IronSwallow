c31d3f6d468b719eb25644129859b705ad572b1d
Evelyn <evelyn@e-s.me.uk>
2020-02-18 08:51:11 +0000

    messages: substitute internal paragraphs with line breaks

--- main.py
+++ main.py
@@ -379,7 +379,7 @@ def store_message(cursor, parsed) -> None:
             # Some messages are enclosed in <p> tags, some have a <p></p> in them.
             # Thank you National Rail, very cool
             pattern = re.compile("^(?:<p>)?([^<]+)(?:</p>)?$")
-            message = pattern.match(message).group(1).replace("<p></p>", "")
+            message = pattern.match(message).group(1).replace("<p></p>", "").replace('</p><p>', '<br>')
 
             if station_list:
                 c.execute("""INSERT INTO darwin_messages VALUES (%s, %s, %s, %s, %s, %s) ON CONFLICT (message_id)
