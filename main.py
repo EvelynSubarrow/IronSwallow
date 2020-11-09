2c4e6048d2ec32d967b78bb9b06734c0c25006fc
Evelyn Snow <git@evelyn.moe>
2020-11-09 23:05:22 +0000

    check to see that locations referenced in inserted assocs also check out

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def store_reference_data(c, parsed) -> None:
     corpus = {a["TIPLOC"]: a for a in corpus}
 
     for reference in parsed["PportTimetableRef"]["list"]:
-        if reference["tag"]=="LocationRef":
+        if reference["tag"] == "LocationRef":
             corpus_loc = corpus.get(reference["tpl"], {})
 
             loc = OrderedDict([
@@ -394,18 +394,26 @@ def store_message(cursor, parsed) -> None:
             else:
                 c.execute("DELETE FROM darwin_messages WHERE message_id=%s;", (record["id"],))
         if record["tag"]=="association":
+            main_owt = full_original_wt(record["main"])
+            assoc_owt = full_original_wt(record["assoc"])
             c.execute("SELECT count(*) FROM darwin_schedules WHERE rid IN %s;", ((record["assoc"]["rid"], record["main"]["rid"]),))
+            c.execute("SELECT count(*) FROM darwin_schedule_locations WHERE rid=%s AND original_wt=%s AND tiploc=%s;", (record["assoc"]["rid"], assoc_owt, record["tiploc"]))
+            c.execute("SELECT count(*) FROM darwin_schedule_locations WHERE rid=%s AND original_wt=%s AND tiploc=%s;", (record["main"]["rid"], main_owt, record["tiploc"]))
             if c.fetchone()[0] != 2:
                 logging.error("Orphan association: main ({}), assoc ({}) cat ({}) loc ({})".format(record["main"]["rid"], record["assoc"]["rid"], record["category"], record["tiploc"]))
+            elif c.fetchone()[0] != 1:
+                logging.error("Orphan loc association: main ({}), assoc (.{}.) cat ({}) loc ({})".format(record["main"]["rid"], record["assoc"]["rid"], record["category"], record["tiploc"]))
+            elif c.fetchone()[0] != 1:
+                logging.error("Orphan loc association: main (.{}.), assoc ({}) cat ({}) loc ({})".format(record["main"]["rid"], record["assoc"]["rid"], record["category"], record["tiploc"]))
             else:
                 if record["category"]=="JJ":
                     # Semantically it makes a lot more sense to invert joins, so that all associations point to the "next" service
                     # JN should hopefully make this distinct from JJ
                     c.execute("""INSERT INTO darwin_associations VALUES (%s, %s, %s, %s, %s, %s) ON CONFLICT(tiploc,main_rid,assoc_rid) DO NOTHING;""",
-                        ("JN", record["tiploc"], record["assoc"]["rid"], full_original_wt(record["assoc"]), record["main"]["rid"], full_original_wt(record["main"])))
+                        ("JN", record["tiploc"], record["assoc"]["rid"], full_original_wt(record["assoc"]), record["main"]["rid"], main_owt))
                 else:
                     c.execute("""INSERT INTO darwin_associations VALUES (%s, %s, %s, %s, %s, %s) ON CONFLICT(tiploc,main_rid,assoc_rid) DO NOTHING;""",
-                        (record["category"], record["tiploc"], record["main"]["rid"], full_original_wt(record["main"]), record["assoc"]["rid"], full_original_wt(record["assoc"])))
+                        (record["category"], record["tiploc"], record["main"]["rid"], full_original_wt(record["main"]), record["assoc"]["rid"], assoc_owt))
 
             # Make sure origin/dest lists are updated as appropriate
             renew_schedule_association_meta(c, record["main"]["rid"], record["assoc"]["rid"])
