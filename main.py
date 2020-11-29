509fb6ec8b347af30fdeb8411c69bfa46b2e4a09
Evelyn Snow <git@evelyn.moe>
2020-11-29 15:55:02 +0000

    stomppy drops disconnect events if it feels like and we should stop trusting it at all

--- main.py
+++ main.py
@@ -102,7 +102,12 @@ def incorporate_ftp(mp) -> None:
 class Listener(stomp.ConnectionListener):
     def __init__(self, mp):
         self.processor = mp
-        self._mq = None
+        self._mq: stomp.Connection = None
+        self.disconnected = True
+        self._attempting_connection = False
+        self._connection_attempts = 0
+        self._connection_attempts_total = 0
+        self._last_connection_attempt = datetime.datetime(1989, 1, 1, 1, 1, 1)
 
     def on_message(self, headers, message):
         try:
@@ -133,16 +138,31 @@ class Listener(stomp.ConnectionListener):
 
     def on_disconnected(self):
         log.error("Disconnected")
-        self.connect_and_subscribe()
+        if not self._attempting_connection:
+            self.disconnected = True
 
     def connect_and_subscribe(self):
-        sleep(10)
+        if not self.disconnected and not self._attempting_connection:
+            return
+
+        self.disconnected = False
+        self._connection_attempts = 0
+        self._attempting_connection = True
+
+        if (datetime.datetime.utcnow()-self._last_connection_attempt).seconds < 10:
+            log.info("Last connection attempt less than 10s ago, delaying")
+            sleep(10)
+
+
         mq = stomp.Connection([(SECRET["hostname"], 61613)],
                               keepalive=True, auto_decode=False, heartbeats=(35000, 35000))
         self._mq = mq
         mq.set_listener("iron-swallow", self)
 
         for n in range(1, 31):
+            self._connection_attempts = n
+            self._connection_attempts_total += 1
+            self._last_connection_attempt = datetime.datetime.utcnow()
             try:
                 log.info("Connecting... (attempt %s)" % n)
                 mq.connect(**{
@@ -158,6 +178,7 @@ class Listener(stomp.ConnectionListener):
                     "activemq.subscriptionName": SECRET["identifier"],
                 })
                 log.info("Connected!")
+                self._attempting_connection = False
                 return
             except Exception as e:
                 backoff = max(min(n ** 2, 600), 10)
@@ -165,6 +186,21 @@ class Listener(stomp.ConnectionListener):
                 log.exception(e)
                 sleep(backoff)
         log.error("Connection attempts exhausted")
+        self.disconnected = True
+        self._attempting_connection = False
+
+    def is_disconnected(self):
+        # Check the STOMP object itself
+        # Check our semaphore
+        # Check the number of connection attempts. If it's 0 we never tried so we're not """disconnected""" from the
+        # main thread's pov
+        # check time elapsed between now and last connection attempt is greater than 5s
+        return (self._mq and not self._mq.is_connected() and self.disconnected and self._connection_attempts and
+                (datetime.datetime.utcnow() - self._last_connection_attempt).seconds > 5)
+
+
+    def is_before_first_connection(self):
+        return not self._mq and self.disconnected and self._connection_attempts_total == 0
 
 
 if __name__ == "__main__":
@@ -204,16 +240,27 @@ if __name__ == "__main__":
                 log.info(f"Waiting for database queue ({mp.count()}) to empty below limit")
                 sleep(10)
 
+            listener = None
             if not SECRET.get("no_listen_stomp"):
                 listener = Listener(mp)
-                listener.connect_and_subscribe()
 
+            tick = 0
             while True:
-                with db_connection.new_cursor() as c2:
-                    ironswallow.store.meta.renew_schedule_meta(c2)
-                for n in range(120*12):
+                tick += 1
+
+                if listener and (listener.is_disconnected() or listener.is_before_first_connection()):
+                    listener.connect_and_subscribe()
+
+                if tick % 3600 == 0:
+                    with db_connection.new_cursor() as c3:
+                        incorporate_reference_data(c3)
+
+                if tick % 3600 == 1:
+                    with db_connection.new_cursor() as c2:
+                        ironswallow.store.meta.renew_schedule_meta(c2)
+
+                if tick % 30 == 0:
                     if mp.count() > 500:
                         log.info(f"Database queue count ({mp.count()}) over limit.")
-                    sleep(30)
-                with db_connection.new_cursor() as c3:
-                    incorporate_reference_data(c3)
+
+                sleep(1)
